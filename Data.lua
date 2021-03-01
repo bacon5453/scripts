fad82bf13ffdf1cd1bdd33610c8272e99bce4cef
Test <73566546+bacon5453@users.noreply.github.com>
2021-02-28 16:24:53 -0800

    Update Data.lua

--- Data.lua
+++ Data.lua
@@ -1,21 +1,14 @@
 return {
 
-["Prison Life"] = {
-version = "2.0"
+	["Prison Life"] = {
+		version = "2.0",
+		
+	},
 
-},
+	["Jailbreak"] = {
+		version = "1.0",
 
-["Bacon Hub"] = {
-version = "1"
-},
+	},
 
-["Owner Acs"] = {
-	[1819892405] = true,
-	[1015575027] = true,
-	[1539183773] = true,
-	[1539156982] = true,
-	[1125412863] = true
-
-}
 
 }
