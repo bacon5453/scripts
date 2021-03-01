d5a86334200d1cb6f88a75966ee9935ab520fdb2
Test <73566546+bacon5453@users.noreply.github.com>
2021-02-28 16:22:29 -0800

    Update Data.lua

--- Data.lua
+++ Data.lua
@@ -2,11 +2,18 @@ return {
 
 ["Prison Life"] = {
 version = "2.0"
-}
- 
 
+},
 
-  
+["Bacon Hub"] = {
+version = "1"
+},
 
-  
-}
+["Owner Acs"] = {
+	[1819892405] = true,
+	[1015575027] = true,
+	[1539183773] = true,
+	[1539156982] = true,
+	[1125412863] = true
+
+}}
