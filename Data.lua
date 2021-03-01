460fe18f31dd7a13e5efca2b9088f97aec9bde64
Test <73566546+bacon5453@users.noreply.github.com>
2021-02-28 16:11:29 -0800

    Update Data.lua

--- Data.lua
+++ Data.lua
@@ -2,6 +2,17 @@ return {
 
 ["Prison Life"] = {
 version = "2.0"
-}
+},
+ 
+["Jailbreak"] = {
+  version = "1"
+  }
 
+  
+  ["Creator Acs"] = {
+  [1124124] = true,
+   [12124] = true,
+   [12124] = true
+  }
+  
 }
