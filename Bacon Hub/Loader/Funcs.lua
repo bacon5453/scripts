89ed0390eda5b870d38ea2c9357db7ffb51a65f0
Test <73566546+bacon5453@users.noreply.github.com>
2021-05-09 05:06:33 -0700

    Update Funcs.lua

--- Bacon Hub/Loader/Funcs.lua
+++ Bacon Hub/Loader/Funcs.lua
@@ -6,17 +6,17 @@ function Funcs:runscript(a)
     gui:Destroy()
 else
     if a == 286090429 then
-        loadstring(game:HttpGet("https://raw.githubusercontent.com/bacon5453/scripts/main/Bacon%20Hub/Arsenal/guiv2.lua"))()
+        loadstring(game:HttpGet("https://raw.githubusercontent.com/bacon5453/scripts/main/Bacon%20Hub/Scripts/Arsenal/guiv2.lua"))()
         wait()
         DestoryUI()
     else
         if a == 155615604 then
-            loadstring(game:HttpGet("https://raw.githubusercontent.com/bacon5453/scripts/main/Bacon%20Hub/Prison%20Life/gui%20v2.lua"))()
+            loadstring(game:HttpGet("https://raw.githubusercontent.com/bacon5453/scripts/main/Bacon%20Hub/Scripts/Prison%20Life/gui%20v2.lua"))()
             wait()
             DestoryUI()
         else
             if a == 1224212277 then
-                loadstring(game:HttpGet("https://raw.githubusercontent.com/bacon5453/scripts/main/Bacon%20Hub/Mad%20City/guiv2.lua"))()
+                loadstring(game:HttpGet("https://raw.githubusercontent.com/bacon5453/scripts/main/Bacon%20Hub/Scripts/Mad%20City/guiv2.lua"))()
                 wait()
                 DestoryUI()
             else
@@ -26,7 +26,7 @@ else
                     DestoryUI()
                 else
                     if a == 301549746 then
-                        loadstring(game:HttpGet("https://raw.githubusercontent.com/bacon5453/scripts/main/Bacon%20Hub/Counter%20Blox/guiv2.lua"))()
+                        loadstring(game:HttpGet("https://raw.githubusercontent.com/bacon5453/scripts/main/Bacon%20Hub/Scripts/Counter%20Blox/guiv2.lua"))()
                         wait()
                         DestoryUI()
                         end
