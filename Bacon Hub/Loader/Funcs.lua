31eddb5cadc0272d51c41497d9a8889759fb0d43
Test <73566546+bacon5453@users.noreply.github.com>
2021-05-09 04:22:18 -0700

    Update Funcs.lua

--- Bacon Hub/Loader/Funcs.lua
+++ Bacon Hub/Loader/Funcs.lua
@@ -29,11 +29,6 @@ else
                         loadstring(game:HttpGet("https://raw.githubusercontent.com/bacon5453/scripts/main/Bacon%20Hub/Counter%20Blox/guiv2.lua"))()
                         wait()
                         DestoryUI()
-                    else
-                        if a == 0 then
-                            --loadstring(game:HttpGet(""))()
-                            wait()
-                            DestoryUI()
                         end
                     end
                 end
@@ -53,10 +48,10 @@ else
         if game.PlaceId == 606849621 then
             getgenv().Gui.Frame.MainFrame.GameName.Text = "Jailbreak"
         else
-            if game.PlaceId == 155615604 then
+            if game.PlaceId == 0 then
                 getgenv().Gui.Frame.MainFrame.GameName.Text = "Mad City"
             else
-                if game.PlaceId == 155615604 then
+                if game.PlaceId == 0 then
                     getgenv().Gui.Frame.MainFrame.GameName.Text = "Counter Blox"
                 else
                     if game.PlaceId == 155615604 then
@@ -64,7 +59,6 @@ else
                     else
                         if game.PlaceId == 0 then
                             --script.Parent.MainFrame.GameName.Text = "Prison Life"
-
                     end
                 end
             end
