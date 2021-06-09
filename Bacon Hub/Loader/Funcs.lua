871e86fb705bf72cf7f5be416a882a1deb01af7b
bacon5453 <73566546+bacon5453@users.noreply.github.com>
2021-06-09 09:56:52 -0700

    Update Funcs.lua

--- Bacon Hub/Loader/Funcs.lua
+++ Bacon Hub/Loader/Funcs.lua
@@ -8,32 +8,32 @@ else
     if a == 286090429 then
         loadstring(game:HttpGet("https://raw.githubusercontent.com/bacon5453/scripts/main/Bacon%20Hub/Scripts/Arsenal/guiv2.lua"))()
         wait()
-        gui:Destroy()
+
     else
         if a == 155615604 then
             loadstring(game:HttpGet("https://raw.githubusercontent.com/bacon5453/scripts/main/Bacon%20Hub/Scripts/Prison%20Life/gui%20v2.lua"))()
             wait()
-           gui:Destroy()
+
         else
             if a == 1224212277 then
                 loadstring(game:HttpGet("https://raw.githubusercontent.com/bacon5453/scripts/main/Bacon%20Hub/Scripts/Mad%20City/guiv2.lua"))()
                 wait()
-                gui:Destroy()
+
             else
                 if a == 0 then
                     --loadstring(game:HttpGet("https://raw.githubusercontent.com/bacon5453/scripts/main/Bacon%20Hub/Arsenal/guiv2.lua"))()
                     wait()
-                    gui:Destroy()
+                    
                 else
                     if a == 301549746 then
                         loadstring(game:HttpGet("https://raw.githubusercontent.com/bacon5453/scripts/main/Bacon%20Hub/Scripts/Counter%20Blox/guiv2.lua"))()
                         wait()
-                       gui:Destroy()
+                       
                             else
                             if a == 1962086868 then
                         loadstring(game:HttpGet("https://raw.githubusercontent.com/bacon5453/scripts/main/Bacon%20Hub/Scripts/Tower%20Of%20Hell/guiv2.lua"))()
                         wait()
-                       gui:Destroy()
+                       
                             
                         end
                     end
@@ -61,7 +61,7 @@ else
                         getgenv().Gui.Frame.MainFrame.GameName.Text = "Prison Life"
                     else
                         if game.PlaceId == 1962086868 then
-                            script.Parent.MainFrame.GameName.Text = "Tower Of Hell"
+                             getgenv().Gui.Frame.MainFrame.GameName.Text = "Tower Of Hell"
                     end
                 end
             end
