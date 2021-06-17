ab0973980e0c22eecc84a9dbe64fac890128dbf8
bacon5453 <73566546+bacon5453@users.noreply.github.com>
2021-06-17 13:35:55 -0700

    Update Functions.lua

--- Bacon Hub/Scripts/Jailbreak/Functions.lua
+++ Bacon Hub/Scripts/Jailbreak/Functions.lua
@@ -1,44 +1,6 @@
 local Functions = {}
 
 
-function Functions:CheckStore(a)
-    if a == "Jewelry" then
-    for i,v in next,game:GetService("Workspace").Jewelrys:GetChildren() do
-    if v:isA("Model") and v.Extra ~= nil then
-    if v.Extra.Sign.Decal.Transparency ~= 0 then
-    return true
-    else
-    return false
-    end
-    end
-    end
-    end
-    if a == "Bank" then
-    for i,v in next,game:GetService("Workspace").Banks:GetChildren() do
-    if v:isA("Model") and v.Extra ~= nil then
-    if v.Extra.Sign.Decal.Transparency ~= 0 then
-    return true
-    else
-    return false
-    end
-    end
-    end
-    end
-    if a == "Museum" then
-        for i,v in next,game.Workspace.Museum.Roof.Hole:GetChildren() do
-            if v:GetChildren()[1] then
-                if v.Transparency == 0 then
-                    return false
-                else
-                    return true
-                end
-            end
-            end
-        end
-    
-    end
-
-
     function Functions:removedamageparts()
         for i,v in next,game:GetService("Workspace").Jewelrys:GetDescendants() do
         if v.Name == "BarbedWire" then
